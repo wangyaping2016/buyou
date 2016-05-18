17ade3477a6a28a4bce3ce169bdf794f0919a3b3
whistlemm <276158636@qq.com>
2016-05-18 13:32:35 +0800

    登录页面，加入了fontawesome图标组件

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+(function(){
+    var designWdith = 7.5;
+    var resizeEvent = 'orientationchange' in window ? 'orientationchange' : 'resize';
+
+    window.addEventListener(resizeEvent, recalc, false);
+    document.documentElement.addEventListener('DOMContentLoaded', recalc, false);
+
+    function recalc(){
+        document.documentElement.style.fontSize = document.documentElement.clientWidth / designWdith + 'px';
+    }
+})
+
